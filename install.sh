9021d41b26b5f501d4811b8d695516013bf35880
HNRobert <hnrobert@qq.com>
2025-07-03 11:21:54 +0800

    fix: syntax

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt update
-sudo apt install  xserver-xorg-core-hwe-18.04\
-sudo apt install  xserver-xorg-video-dummy-hwe-18.04
+sudo apt install xserver-xorg-core-hwe-18.04
+sudo apt install xserver-xorg-video-dummy-hwe-18.04
 
 sudo cp ./99-dummy.conf /usr/share/X11/xorg.conf.d/99-dummy.conf
 echo "Installation successful! Please reboot your system for the virtual screen configuration to take effect."
